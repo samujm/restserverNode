f06558afe1d3f3f117cef3a2242019ba23dddaa8
Samantha González <samujm76@gmail.com>
2022-11-12 15:39:06 -0600

    Login de usuario, controlador de login (comprobar que exista el usuario, su status y la contraseña)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,54 @@
 const { response } = require("express");
+const Usuario = require('../models/usuario');
+const bcryptjs = require('bcryptjs');
 
-const login = (req, res = response)=>{
-    res.json({
-        msg: 'Login ok'
-    });
+
+const login = async(req, res = response)=>{
+
+    const { correo, password } = req.body;
+
+    try {
+        //Verificar si el email existe
+        const usuario = await Usuario.findOne({ correo });
+        if(!usuario){
+            return res.status(400).json({
+                msg:'Usuario / Password no son correctos - correo',
+            });
+        }
+
+        //Verificar si en usuario está activo en la base
+        if(!usuario.estado){
+            return res.status(400).json({
+                msg:'Usuario / Password no son correctos - estado: false',
+            });
+        }
+
+        //Verificar la contraseña
+        const validPassword = bcryptjs.compareSync( password, usuario.password );
+        if ( !validPassword ) {
+            return res.status(400).json({
+                msg:'Usuario / Password no son correctos - password',
+            });
+        }
+
+
+
+        //General el JWT
+
+
+
+        //Solo se debe ejecutar un solo res.json en todo el flujo del controlador
+        res.json({
+            msg: 'Login ok',
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable con  el administrador'
+        });
+    }
+
 }
 
 module.exports = {
